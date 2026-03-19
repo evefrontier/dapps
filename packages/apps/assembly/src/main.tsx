62c63af60513e81e8a3433233968bc60a5e2c1c1
ccp sondheim <sondheim@ccpgames.com>
2026-03-19 16:24:01 +0000

    rm unused tracing

--- packages/apps/assembly/src/main.tsx
+++ packages/apps/assembly/src/main.tsx
@@ -1,6 +1,4 @@
-import { HoneycombWebSDK } from "@honeycombio/opentelemetry-web";
 import { ThemeProvider, createTheme } from "@mui/material";
-import { getWebAutoInstrumentations } from "@opentelemetry/auto-instrumentations-web";
 import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -19,26 +17,6 @@ import { QueryClient } from "@tanstack/react-query";
 const configDefaults = {
   ignoreNetworkEvents: false,
 };
-
-const sdk = new HoneycombWebSDK({
-  debug: true, // Set to false for production environment.
-  apiKey: import.meta.env.VITE_HONEYCOMB_API_KEY,
-  serviceName: "assembly-dapp",
-  sampleRate: 500, // Sample 1 in 500 events
-  instrumentations: [
-    getWebAutoInstrumentations({
-      "@opentelemetry/instrumentation-xml-http-request": configDefaults,
-      "@opentelemetry/instrumentation-fetch": configDefaults,
-      "@opentelemetry/instrumentation-document-load": {
-        ignoreNetworkEvents: false,
-      },
-      "@opentelemetry/instrumentation-user-interaction": {
-        eventNames: ["click", "submit"],
-      },
-    }),
-  ],
-});
-sdk.start();
 
 const darkTheme = createTheme({
   palette: {
